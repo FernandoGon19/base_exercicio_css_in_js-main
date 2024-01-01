fff050aa9d50391b25bc2643ab60fd112d6e80e2
FernandoGon19 <129014797+FernandoGon19@users.noreply.github.com>
2024-01-01 11:49:52 -0300

    Atualizando arquivos

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,13 @@
+import { FormularioHero, HeroTittle } from './styles'
+
+const Hero = () => (
+  <FormularioHero>
+    <div className="container">
+      <HeroTittle>
+        As melhores vagas para tecnologia, design e artes visuais.
+      </HeroTittle>
+    </div>
+  </FormularioHero>
+)
+
+export default Hero
